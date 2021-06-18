6c7b2a06392048f94261dc0262f2959ac3199724
Moiz Ali Moomin <moiz.7152@gmail.com>
2021-06-18 13:48:47 +0530

    added

---
+++ main.js
@@ -0,0 +1,11 @@
+function lw() {
+    var i=document.getElementById("input").value 
+    var xhr = new XMLHttpRequest()
+    
+    xhr.open("GET","http://172.22.176.24/cgi-bin/linux.py?cmd="+i,true);
+    xhr.send();
+    xhr.onload = function(){
+         var output = xhr.responseText;
+         document.getElementById("d1").innerHTML=output;
+    }
+}
